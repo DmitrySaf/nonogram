df7a22fded689b643d4c5506664b545416d08a0a
DmitrySaf <dmitry.study@yandex.ru>
2022-10-11 17:11:50 +0300

    added health bar

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 
 import Table from "../Table/Table";
 import ModeChanger from "../ModeChanger/ModeChanger";
+import HealthBar from "../HealthBar/HealthBar";
 
 import * as data from '../../assets/levels-data-list.json';
 import './App.scss';
@@ -12,6 +13,7 @@ function App() {
   return (
     <div className="container">
       <h1>Nonogram</h1>
+      <HealthBar />
       <Table level={levels[0]} />
       <ModeChanger />
     </div>
---
+++ src/components/HealthBar/HealthBar.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './HealthBar.scss';
+
+function HealthBar() {
+  return (
+    <div className="health-bar">
+      <div className="health-bar__heart">favorite</div>
+      <div className="health-bar__heart">favorite</div>
+      <div className="health-bar__heart">favorite</div>
+    </div>
+  );
+}
+
+export default HealthBar;
