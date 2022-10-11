b432bc52786a1b309068f7bbeaaef0b67f7dc98f
DmitrySaf <dmitry.study@yandex.ru>
2022-10-11 12:20:52 +0300

    initialized the app, organized the structure

---
+++ src/components/App/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Table from "../Table/Table";
+
+import './App.scss';
+
+function App() {
+  return (
+    <div>
+      <h1>Nonogram</h1>
+      <Table />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Table/Table.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Table() {
+  return (
+    <div>
+      <table>
+        <tbody>
+          <th></th>
+          <th></th>
+          <th></th>
+          <th></th>
+          <th></th>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Table;
