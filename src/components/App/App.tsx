f7affe70bc8cbcd79e9501a9c7e22d0f6586ad35
DmitrySaf <dmitry.study@yandex.ru>
2022-10-11 16:55:11 +0300

    removed packages for json server and moved the file. Now should work on gh-pages

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import { useState } from 'react';
 
 import Table from "../Table/Table";
+import ModeChanger from "../ModeChanger/ModeChanger";
 
+import * as data from '../../assets/levels-data-list.json';
 import './App.scss';
 
 function App() {
+  const [levels, setLevels] = useState(data.levels);
+
   return (
     <div className="container">
       <h1>Nonogram</h1>
-      <Table />
+      <Table level={levels[0]} />
+      <ModeChanger />
     </div>
   );
 }
