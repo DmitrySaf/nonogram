f72ad73749e124b0a8faf4a71b7b9bf3d1f44b5e
DmitrySaf <dmitry.study@yandex.ru>
2022-10-13 15:21:54 +0300

    added colors changing on completion

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,3 +1,5 @@
+import classNames from "classnames";
+
 import Row from './Row';
 import KeysRow from "./KeysRow";
 
@@ -7,13 +9,19 @@ import useTypedSelector from '../../hooks/useTypedSelector';
 interface ILevel {
   id: number,
   name: string,
-  code: string
+  code: string,
+  colorsCode: string,
+  colors: string[]
 }
 
 function Table({ level }: { level: ILevel}) {
   const levelCode = level?.code || '';
   const size = Math.sqrt(levelCode.length);
   const { accomplishment } = useTypedSelector(state => state);
+  const bodyClasses = classNames({
+    'table__body': true,
+    'table__body_colored': accomplishment.match(/1/g)?.length === levelCode.match(/1/g)?.length
+  })
 
   const getEveryTenthValue = (string: string, start: number) => {
     const result = [];
@@ -27,7 +35,7 @@ function Table({ level }: { level: ILevel}) {
 
   return (
     <table className="table">
-      <tbody className="table__body">
+      <tbody className={bodyClasses}>
         <tr>
           <td></td>
           {
@@ -37,13 +45,34 @@ function Table({ level }: { level: ILevel}) {
           }
         </tr>
         {
-          [...Array(size)].map((x, i) =>
-            <Row accomplishment={getRowCode(accomplishment, i)} size={size} rowCode={getRowCode(levelCode, i)} index={i} key={i} />
-          )
+          accomplishment.match(/1/g)?.length === levelCode.match(/1/g)?.length 
+            ? <TableColored code={level?.colorsCode} colors={level?.colors}/>
+            : [...Array(size)].map((x, i) =>
+              <Row accomplishment={getRowCode(accomplishment, i)} size={size} rowCode={getRowCode(levelCode, i)} index={i} key={i} />
+            )
         }
       </tbody>
     </table>
   );
+}
+
+const TableColored = ({code, colors}: {code: string, colors: string[]}) => {
+  return (
+    <>
+    {
+          [...Array(10)].map((x, i) =>
+            <tr key={i}>
+              <td className="table__keys table__keys_theme_row table__keys_accomplished">10</td>
+              {[...Array(10)].map((x, j) => {
+                console.log(code.length, i*10 + j)
+                return <td key={j} width={40} height={40} style={{backgroundColor: colors[parseInt(code[i*10 + j])]}} ></td>
+              })}
+            </tr>
+          )
+        }
+    </>
+        
+  )
 }
 
 export default Table;
