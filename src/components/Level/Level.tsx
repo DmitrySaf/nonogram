bd1251a510bf8a60a0b19d9c3a8ce4b960ec29f2
DmitrySaf <dmitry.study@yandex.ru>
2022-10-14 17:25:15 +0300

    added a return button

--- src/components/Level/Level.tsx
+++ src/components/Level/Level.tsx
@@ -1,9 +1,12 @@
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import Table from "../Table/Table";
 import ModeChanger from "../ModeChanger/ModeChanger";
 import HealthBar from "../HealthBar/HealthBar";
 import RetryModal from "../RetryModal/RetryModal";
+
+import './Level.scss';
 
 function Level({ levels }: {levels: any}) {
   const { id } = useParams();
@@ -11,6 +14,12 @@ function Level({ levels }: {levels: any}) {
 
   return (
     <>
+      <div className="level__header">
+        <h2 className="level__title">Level {id}</h2>
+        <Link to=".." className="level__link">
+
+        </Link>
+      </div>
       <HealthBar/>
       <Table level={level}/>
       <ModeChanger/>
