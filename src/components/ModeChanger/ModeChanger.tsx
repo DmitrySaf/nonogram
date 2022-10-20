3647a8e6e4f07023d777d433e93f65b0a7bebc88
DmitrySaf <dmitry.study@yandex.ru>
2022-10-20 13:20:47 +0300

    added a hint mode

--- src/components/ModeChanger/ModeChanger.tsx
+++ src/components/ModeChanger/ModeChanger.tsx
@@ -1,13 +1,26 @@
 import { useDispatch } from 'react-redux';
+import classNames from "classnames";
 
 import useTypedSelector from '../../hooks/useTypedSelector';
 import { setMode } from '../../store/slices/LevelSlice';
 
+import bulb from '../../assets/img/bulb.png';
 import './ModeChanger.scss';
 
 function ModeChanger() {
   const { mode } = useTypedSelector((state) => state);
   const dispatch = useDispatch();
+  const hintClassnames = classNames({
+    'mode-changer__button': true,
+    'mode-changer__button-hint': true,
+    'mode-changer__button-hint_equipped': mode === 'hint'
+  });
+  const modeWrapperClassnames = classNames({
+    'mode-changer__mode-wrapper': true,
+    'mode-changer__mode-wrapper_equipped': mode !== 'hint',
+    'mode-changer__mode-wrapper_mode_cross': mode === 'cross',
+    'mode-changer__mode-wrapper_mode_block': mode === 'block'
+  });
 
   const onModeToggle = () => {
     const newMode = (mode === 'block') ? 'cross' : 'block';
@@ -15,12 +28,18 @@ function ModeChanger() {
     dispatch(setMode(newMode));
   };
 
+  const onHintClick = () => {
+    dispatch(setMode('hint'));
+  }
+
   return (
     <div className="mode-changer">
-      <input className="mode-changer__input" type="checkbox" checked={mode === 'block'} readOnly />
-      <button className="mode-changer__mode-grid" type="button" onClick={onModeToggle}>
-        <div className="mode-changer_mode_cross" />
-        <div className="mode-changer_mode_block" />
+      <div className={modeWrapperClassnames} onClick={onModeToggle}>
+        <button className="mode-changer__button mode-changer__button-cross" />
+        <button className="mode-changer__button mode-changer__button-block" />
+      </div>
+      <button className={hintClassnames} data-hints={3} onClick={onHintClick}>
+        <img src={bulb} alt="hint" className="mode-changer__hint-icon" />
       </button>
     </div>
   );
